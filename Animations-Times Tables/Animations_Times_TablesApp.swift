2e37af0e265f7b12f804459692bbf65bd446636c
randymesso <Randymesso@gmail.com>
2025-09-09 19:13:17 -0500

    Initial Commit

---
+++ Animations-Times Tables/Animations_Times_TablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animations_Times_TablesApp.swift
+//  Animations-Times Tables
+//
+//  Created by Randy Messo on 9/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Animations_Times_TablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
